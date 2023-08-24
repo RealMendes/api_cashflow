540f9f004ba4300d97278cd6ad389dc16167ab7b
Letícia Gabriela <70960662+RealMendes@users.noreply.github.com>
2023-08-24 19:20:20 -0300

    Alterações para rodar no heroku

--- api/__init__.py
+++ api/__init__.py
@@ -4,22 +4,27 @@ from flask_migrate import Migrate
 from flask_marshmallow import Marshmallow
 from flask_restx import Api
 
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config')
 
-app = Flask(__name__)
-app.config.from_object('config')
+    db = SQLAlchemy(app)
+    ma = Marshmallow(app)
+    mi = Migrate(app, db)
 
-db = SQLAlchemy(app)
-ma = Marshmallow(app)
-mi = Migrate(app, db)
+    api = Api(app, version='1.0', title='CashFlow', description='Financial control API')
 
-api = Api(app, version='1.0', title='CashFlow', description='Financial control API')
+    from .models import account_model
+    from .views import account_view
 
+    from .models import transaction_model
+    from .views import transaction_view
 
-from .models import account_model
-from .views import account_view
+    from .models import user_model
+    from .views import user_view
 
-from .models import transaction_model
-from .views import transaction_view
+    return app
 
-from .models import user_model
-from .views import user_view
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
